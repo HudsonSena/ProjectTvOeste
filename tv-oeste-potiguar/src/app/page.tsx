57b69c5cf9b243c8b08e261e7697e0324c7ff3a7
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-10-18 12:48:27 -0300

    updae theme light and dark mode

---
+++ tv-oeste-potiguar/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@/components/modeToggle";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-primary">Hello World</h1>
+      <ModeToggle />
+    </div>
+  );
+}
