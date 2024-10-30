7fc81b162fdd71ce79e9495ec74b986d41c63624
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-10-30 17:03:46 -0300

    update responsive header

--- tv-oeste-potiguar/src/app/_components/header.tsx
+++ tv-oeste-potiguar/src/app/_components/header.tsx
@@ -5,39 +5,47 @@ import { ModeToggle } from "./modeToggle";
 
 export function Header() {
     return (
-        <header id="header" className="p-5 sm:p-0 sm:w-[80%] h-28 border m-auto flex items-center justify-between relative">
-            <div className="m-auto">
-                <h1 className="font-bold text-xl">TV OESTE RN</h1>
-                <p>Desde 2018</p>
+        <header id="header" className="py-5 sm:p-0 lg:px-80 mx-auto flex flex-col items-center gap-2 justify-between shadow-sm">
+            <div className="w-full hidden pt-5 lg:flex">
+                <nav className="w-full flex flex-row justify-around text-primary">
+                    <a href="#" className="hover:underline" >Notíciais</a>
+                    <a href="#" className="hover:underline" >Eventos</a>
+                    <a href="#" className="hover:underline" >Serviços</a>
+                    <a href="#" className="hover:underline" >Contatos</a>
+                </nav>
             </div>
-            <div className="absolute left-3">
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" size="icon">
-                            <LucideMenu className="h-[1.2rem] w-[1.2rem] transition-all text-primary" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem>
-                            Notícias
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            Eventos
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            Serviços
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            Contatos
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+            <div className="flex flex-row justify-between">
+                <div>
+                    <h1 className="font-bold text-2xl lg:text-8xl">TV OESTE RN</h1>
+                    <div className="flex justify-end items-center gap-3">
+                        <p className="font-semibold text-primary ">Desde 2018</p>
+                        <ModeToggle />
+                    </div>
+                </div>
+                <div className="absolute left-3 lg:hidden">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <LucideMenu className="h-[1.2rem] w-[1.2rem] transition-all text-primary" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem>
+                                Notícias
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Eventos
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Serviços
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Contatos
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </div>
-            <div className="absolute right-3">
-                <ModeToggle />
-            </div>
-
-
         </header>
     )
 }
