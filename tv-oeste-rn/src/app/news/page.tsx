8f8dc1eca0adcb1699a680e5908b1536eb922de5
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-11-05 16:22:58 -0300

    update routes and naigations

---
+++ tv-oeste-rn/src/app/news/page.tsx
@@ -0,0 +1,9 @@
+export default function News() {
+    return (
+        <div>
+            News Pages
+
+            <a href="/">Home</a><a href="/news">News</a><a href=";events">Events</a>
+        </div>
+    )
+}
